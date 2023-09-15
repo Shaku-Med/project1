475a3d979784a81427cec3d4587d64bea8789653
Medzy Ems Amara <100793520+Shaku-Med@users.noreply.github.com>
2023-09-14 22:04:13 -0400

    Update Routing.jsx

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -5,7 +5,6 @@ import io from 'socket.io-client'
 import Profile from './Components/Profile/Profile'
 import Preview from './Components/Preview/Preview'
 
-let socket = io('https://project1backend.badzybaddest.repl.co')
 function Routing() {
   return (
    <Routes>
